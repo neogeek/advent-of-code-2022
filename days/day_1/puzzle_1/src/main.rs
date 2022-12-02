84de8f6eea6ccf5ccd01401bba5e1ec883da1016
Scott Doxey <neogeek0101@gmail.com>
2022-12-01 22:03:50 -0500

    Added rust attempt.

--- days/day_1/puzzle_1/src/main.rs
+++ days/day_1/puzzle_1/src/main.rs
@@ -1,3 +1,21 @@
+use regex::Regex;
+use std::collections::HashMap;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let puzzle_input =
+        fs::read_to_string("input.txt").expect("Should be able to load the puzzle input.");
+
+    let group_re = Regex::new(r"\n{2,}").unwrap();
+    let item_re = Regex::new(r"\n").unwrap();
+
+    let mut elfs = HashMap::new();
+
+    let mut index = 1;
+
+    for group in group_re.split(&puzzle_input) {
+        elfs.insert(index, item_re.split(group));
+
+        index += 1;
+    }
 }
