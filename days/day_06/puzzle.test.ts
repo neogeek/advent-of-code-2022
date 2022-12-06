7fd2e8dff46ab6e6b48a555d84e28e41c4894f0c
Scott Doxey <neogeek0101@gmail.com>
2022-12-06 00:20:28 -0500

    Added part 1.

--- days/day_06/puzzle.test.ts
+++ days/day_06/puzzle.test.ts
@@ -3,17 +3,19 @@ import { describe } from 'https://deno.land/std@0.167.0/testing/bdd.ts';
 
 import { calculatePart1, calculatePart2 } from './puzzle.ts';
 
-const example = Deno.readTextFileSync('days/day_06/example.txt');
-
 describe('day 6', () => {
   describe('part 1', () => {
     Deno.test('returns the expected result', () => {
-      assertEquals(calculatePart1(example), -1);
+      assertEquals(calculatePart1('mjqjpqmgbljsphdztnvjfqwrcgsmlb'), 7);
+      assertEquals(calculatePart1('bvwbjplbgvbhsrlpgdmjqwftvncz'), 5);
+      assertEquals(calculatePart1('nppdvjthqldpwncqszvftbrmjlhg'), 6);
+      assertEquals(calculatePart1('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg'), 10);
+      assertEquals(calculatePart1('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw'), 11);
     });
   });
   describe('part 2', () => {
     Deno.test('returns the expected result', () => {
-      assertEquals(calculatePart2(example), -1);
+      assertEquals(calculatePart2(''), -1);
     });
   });
 });
--- days/day_06/puzzle.ts
+++ days/day_06/puzzle.ts
@@ -1,4 +1,14 @@
 export const calculatePart1 = (input: string) => {
+  const characters = input.trim().split('');
+
+  for (let i = 0; i < characters.length; i += 1) {
+    const packet = characters.slice(i, i + 4);
+
+    if (packet.length === Array.from(new Set([...packet])).length) {
+      return i + 4;
+    }
+  }
+
   return 0;
 };
 
