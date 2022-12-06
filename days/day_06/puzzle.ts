f3709950af916e8d3520e375f39ec34ad84714c1
Scott Doxey <neogeek0101@gmail.com>
2022-12-06 00:28:21 -0500

    Added reusable method.

--- days/day_06/puzzle.ts
+++ days/day_06/puzzle.ts
@@ -1,7 +1,5 @@
-export const calculatePart1 = (input: string) => {
+const findStartOfPacketMarker = (input: string, packetLength: number) => {
   const characters = input.trim().split('');
-
-  const packetLength = 4;
 
   for (let i = 0; i < characters.length; i += 1) {
     const packet = characters.slice(i, i + packetLength);
@@ -14,18 +12,10 @@ export const calculatePart1 = (input: string) => {
   return 0;
 };
 
+export const calculatePart1 = (input: string) => {
+  return findStartOfPacketMarker(input, 4);
+};
+
 export const calculatePart2 = (input: string) => {
-  const characters = input.trim().split('');
-
-  const packetLength = 14;
-
-  for (let i = 0; i < characters.length; i += 1) {
-    const packet = characters.slice(i, i + packetLength);
-
-    if (packet.length === Array.from(new Set([...packet])).length) {
-      return i + packetLength;
-    }
-  }
-
-  return 0;
+  return findStartOfPacketMarker(input, 14);
 };
