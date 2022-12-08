37b54e68b276c689a27eb43ad336d3727b4fa569
Scott Doxey <neogeek0101@gmail.com>
2022-12-07 23:42:19 -0500

    Added empty day 8.

---
+++ days/day_08/puzzle.test.ts
@@ -0,0 +1,19 @@
+import { assertEquals } from 'https://deno.land/std@0.167.0/testing/asserts.ts';
+import { describe } from 'https://deno.land/std@0.167.0/testing/bdd.ts';
+
+import { calculatePart1, calculatePart2 } from './puzzle.ts';
+
+const example = Deno.readTextFileSync('days/day_08/example.txt');
+
+describe('day 8', () => {
+  describe('part 1', () => {
+    Deno.test('returns the expected result', () => {
+      assertEquals(calculatePart1(example), -1);
+    });
+  });
+  describe('part 2', () => {
+    Deno.test('returns the expected result', () => {
+      assertEquals(calculatePart2(example), -1);
+    });
+  });
+});
