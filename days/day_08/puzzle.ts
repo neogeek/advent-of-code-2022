a9a2f81914437079f2b23a3cb86e55d063cff7a6
Scott Doxey <neogeek0101@gmail.com>
2022-12-08 02:08:20 -0500

    Added part 2.

--- days/day_08/puzzle.ts
+++ days/day_08/puzzle.ts
@@ -1,5 +1,10 @@
 import * as mod from 'https://deno.land/std@0.167.0/uuid/mod.ts';
 
+interface Tree {
+  uuid: string;
+  height: number;
+}
+
 const parseTrees = (input: string) => {
   const rows = input
     .trim()
@@ -11,7 +16,7 @@ const parseTrees = (input: string) => {
       }))
     );
 
-  const cols = new Array(rows[0].length);
+  const cols: Tree[][] = new Array(rows[0].length);
 
   cols.fill([]).map((_, index) => {
     cols[index] = rows.map(row => row[index]);
@@ -20,30 +25,72 @@ const parseTrees = (input: string) => {
   return { rows, cols };
 };
 
-const checkForHeight = (items: { uuid: string; height: number }[]) => {
+const checkForHeight = (items: Tree[]) => {
   return items.filter((item, index) => {
     return !items.slice(0, index).some(prev => prev.height >= item.height);
   });
 };
 
+const checkForDistance = (items: Tree[], height: number) => {
+  const tallerTrees = items.findIndex(item => item.height >= height);
+
+  if (tallerTrees === -1) return items.length;
+
+  return items.slice(0, tallerTrees + 1).length;
+};
+
 export const calculatePart1 = (input: string) => {
   const { rows, cols } = parseTrees(input);
 
-  const visible: { uuid: string; height: number }[] = [];
+  const visible: Tree[] = [];
 
   rows.map(row => {
     checkForHeight(row).map(item => visible.push(item));
-    checkForHeight(row.reverse()).map(item => visible.push(item));
+    checkForHeight([...row].reverse()).map(item => visible.push(item));
   }, 0);
 
   cols.map(l => {
     checkForHeight(l).map(item => visible.push(item));
-    checkForHeight(l.reverse()).map(item => visible.push(item));
+    checkForHeight([...l].reverse()).map(item => visible.push(item));
   }, 0);
 
   return Array.from(new Set(visible)).length;
 };
 
 export const calculatePart2 = (input: string) => {
-  return 0;
+  const { rows, cols } = parseTrees(input);
+
+  const scores: number[] = [];
+
+  rows.map((_, rowIndex) =>
+    cols.map((_, colIndex) => {
+      const item = rows[rowIndex][colIndex];
+
+      const top = checkForDistance(
+        cols[colIndex].slice(0, rowIndex),
+        item.height
+      );
+
+      const left = checkForDistance(
+        rows[rowIndex].slice(0, colIndex),
+        item.height
+      );
+
+      const right = checkForDistance(
+        [...rows[rowIndex]].reverse().slice(colIndex + 1),
+        item.height
+      );
+
+      const bottom = checkForDistance(
+        [...cols[colIndex]].reverse().slice(rowIndex + 1),
+        item.height
+      );
+
+      const score = top * left * right * bottom;
+
+      scores.push(score);
+    })
+  );
+
+  return [...scores.sort()].reverse()[0];
 };
