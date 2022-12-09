201f05a1edb3ac688951a058cbf7218adbf0f9db
Scott Doxey <neogeek0101@gmail.com>
2022-12-09 02:11:48 -0500

    Added part 1.

--- days/day_09/puzzle.ts
+++ days/day_09/puzzle.ts
@@ -1,5 +1,82 @@
+import type { Position } from '../../types.ts';
+
+interface Step {
+  position: Position;
+}
+
 export const calculatePart1 = (input: string) => {
-  return 0;
+  const instructions = input.trim().split('\n');
+
+  const currentHeadPosition: Position = { x: 0, y: 0 };
+  const currentTailPosition: Position = { x: 0, y: 0 };
+
+  const headSteps: Step[] = [{ position: { ...currentTailPosition } }];
+  const tailSteps: Step[] = [{ position: { ...currentTailPosition } }];
+
+  for (let i = 0; i < instructions.length; i += 1) {
+    const [direction, numSteps] = instructions[i].split(' ');
+
+    const parsedNumSteps = parseInt(numSteps);
+
+    for (let j = 0; j < parsedNumSteps; j += 1) {
+      if (direction === 'R') {
+        currentHeadPosition.x++;
+      } else if (direction === 'L') {
+        currentHeadPosition.x--;
+      } else if (direction === 'D') {
+        currentHeadPosition.y--;
+      } else if (direction === 'U') {
+        currentHeadPosition.y++;
+      }
+
+      if (currentHeadPosition.x - currentTailPosition.x > 1) {
+        if (currentTailPosition.y != currentHeadPosition.y) {
+          currentTailPosition.y = currentHeadPosition.y;
+        }
+        currentTailPosition.x = currentHeadPosition.x - 1;
+      }
+
+      if (currentHeadPosition.x - currentTailPosition.x < -1) {
+        if (currentTailPosition.y != currentHeadPosition.y) {
+          currentTailPosition.y = currentHeadPosition.y;
+        }
+        currentTailPosition.x = currentHeadPosition.x + 1;
+      }
+
+      if (currentHeadPosition.y - currentTailPosition.y > 1) {
+        if (currentTailPosition.x != currentHeadPosition.x) {
+          currentTailPosition.x = currentHeadPosition.x;
+        }
+        currentTailPosition.y = currentHeadPosition.y - 1;
+      }
+
+      if (currentHeadPosition.y - currentTailPosition.y < -1) {
+        if (currentTailPosition.x != currentHeadPosition.x) {
+          currentTailPosition.x = currentHeadPosition.x;
+        }
+        currentTailPosition.y = currentHeadPosition.y + 1;
+      }
+
+      headSteps.push({ position: { ...currentHeadPosition } });
+      tailSteps.push({ position: { ...currentTailPosition } });
+    }
+  }
+
+  const uniqueSteps: Step[] = [];
+
+  tailSteps.map(tailStep => {
+    if (
+      !uniqueSteps.some(
+        uniqueStep =>
+          uniqueStep.position.x === tailStep.position.x &&
+          uniqueStep.position.y === tailStep.position.y
+      )
+    ) {
+      uniqueSteps.push(tailStep);
+    }
+  });
+
+  return uniqueSteps.length;
 };
 
 export const calculatePart2 = (input: string) => {
