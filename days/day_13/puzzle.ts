e8e2171813e44613d40ffeb2717735eb30b5b79c
Scott Doxey <neogeek0101@gmail.com>
2022-12-17 14:20:35 -0500

    Added missing days 12 - 17.

---
+++ days/day_13/puzzle.ts
@@ -0,0 +1,7 @@
+export const calculatePart1 = (input: string) => {
+  return 0;
+};
+
+export const calculatePart2 = (input: string) => {
+  return 0;
+};
---
+++ days/day_15/puzzle.test.ts
@@ -0,0 +1,19 @@
+import { assertEquals } from 'https://deno.land/std@0.167.0/testing/asserts.ts';
+import { describe, it } from 'https://deno.land/std@0.167.0/testing/bdd.ts';
+
+import { calculatePart1, calculatePart2 } from './puzzle.ts';
+
+const example = Deno.readTextFileSync('days/day_17/example.txt');
+
+describe('day 15', () => {
+  describe('part 1', () => {
+    it.ignore('returns the expected result', () => {
+      assertEquals(calculatePart1(example), -1);
+    });
+  });
+  describe('part 2', () => {
+    it.ignore('returns the expected result', () => {
+      assertEquals(calculatePart2(example), -1);
+    });
+  });
+});
